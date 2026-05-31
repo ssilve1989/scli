83c9e6e9f85f67dd2dfdef5d0901b58de36b7049
Steven Silvestri <steven.m.silvestri@gmail.com>
2026-05-31 16:32:34 -0600

    ci: update workflows for Rust build matrix, update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 set -e
-bun install && bun run build && ln -sf "$(pwd)/dist/scli" ~/.local/bin/scli
+mise install
+cargo build --release
+cp target/release/scli ~/.local/bin/scli
 echo "Installed scli to ~/.local/bin/scli"
